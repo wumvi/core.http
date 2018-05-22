956fba233661adfa91ab15f473aa5eeaec60a84f
wumvi <vk@wumvi.com>
2018-05-23 00:58:37 +0300

    Update index.php

--- script/index.php
+++ script/index.php
@@ -93,10 +93,8 @@ try {
 
     exit;
 } catch (\Throwable $t) {
-    http_response_code(500);
+    // http_response_code(500);
     // To get 500 error, should set ini_set('display_errors', 'Off');
 
-    if ($runMode === Init::DEV_MODE_DEV) {
-        throw $t;
-    }
+    throw $t;
 }
